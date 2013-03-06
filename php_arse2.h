d9cfcdaf8c36717074d7a787ec6b1af3652796ca
Leigh Simpson <code@simpleigh.com>
2013-03-06 22:13:22 +0000

    Removed unneeded header file declaration

--- php_arse2.h
+++ php_arse2.h
@@ -4,14 +4,6 @@
 extern zend_module_entry arse2_module_entry;
 #define phpext_arse2_ptr &arse2_module_entry
 
-#ifdef PHP_WIN32
-#	define PHP_ARSE2_API __declspec(dllexport)
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#	define PHP_ARSE2_API __attribute__ ((visibility("default")))
-#else
-#	define PHP_ARSE2_API
-#endif
-
 #ifdef ZTS
 #include "TSRM.h"
 #endif
